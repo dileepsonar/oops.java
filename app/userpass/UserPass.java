9c7dcf904aed80bec0dca96b3780fae9e94c4008
unknown <dileepsonar342@gmail.com>
2022-10-07 06:43:55 +0530

    first class

---
+++ app/userpass/UserPass.java
@@ -0,0 +1,23 @@
+package com.progrm.app.userpass;
+import java.util.*;
+public class UserPass {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+	
+	String user, pass;
+
+	System.out.print("Enter your username ");
+	user = input.nextLine();
+
+	
+	if(user.equals("dkkk") && (pass.equals("Jedi")))
+	{ 
+		System.out.println("Welcome to dkkk.");
+
+	}else{
+
+	System.out.println("These uset and password wrong.");
+
+	
+}
+	}}
